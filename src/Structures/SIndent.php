f7f2951a7a5a7fa3779e899f591467f91f51b96d
hacpaka <hacpaka@eggbe.com>
2018-11-24 05:42:52 +0300

    +

--- src/Structures/SIndent.php
+++ src/Structures/SIndent.php
@@ -9,6 +9,7 @@ use \Exception;
 /**
  * @property int level
  * @property int size
+ * @property bool changed
  * @property bool increased
  * @property bool decreased
  * @property string text
@@ -19,8 +20,13 @@ class SIndent extends AStruct {
 	 * @var array
 	 */
 	protected static array $Prototype = [
-		'level', 'text', 'increased', 'decreased',
+		'level', 'text', 'changed', 'increased', 'decreased',
 	];
+
+	/**
+	 * @var bool
+	 */
+	protected const defaultChangedValue = false;
 
 	/**
 	 * @var bool
@@ -36,6 +42,14 @@ class SIndent extends AStruct {
 	 * @var bool
 	 */
 	protected const defaultLevelValue = 0;
+
+	/**
+	 * @param bool $value
+	 * @return bool
+	 */
+	public final function setChangedProperty(bool $value): bool {
+		return $value;
+	}
 
 	/**
 	 * @param bool $value
@@ -64,9 +78,10 @@ class SIndent extends AStruct {
 	 */
 	public final function analize(string $indent): void {
 		if (preg_match('/[^\t]+/', $indent)) {
-			_dumpe($indent);
 			throw new Exception('Incorrect indentation characters!');
 		}
+
+		$this->changed = false;
 
 		$this->decreased = false;
 		$this->increased = false;
@@ -74,30 +89,18 @@ class SIndent extends AStruct {
 		if (strlen($indent) > Arr::last($this->Stack)) {
 			array_push($this->Stack, strlen($indent));
 
+			$this->changed = true;
 			$this->increased = true;
 			$this->level++;
 		} else {
-			if (strlen($indent) < Arr::last($this->Stack)) {
+
+			while (strlen($indent) < Arr::last($this->Stack)) {
 				array_pop($this->Stack);
 
-				if (strlen($indent) <= Arr::last($this->Stack)) {
-					$this->decreased = true;
-					$this->level--;
-
-				} else {
-					array_push($this->Stack, strlen($indent));
-				}
+				$this->changed = true;
+				$this->decreased = true;
+				$this->level--;
 			}
 		}
  	}
-
-	/*
-	public final function debuge() {
-		echo sprintf("DEBUG: increased: %s, decreased: %s, level: %s\n",
-			$this->increased ? 'true' : 'false',
-			$this->decreased ? 'true' : 'false',
-			$this->level
-		);
- 	}
-	*/
 }
